5baa74a48dca35a681d1c1085e673a5aaab19750
faithremo <faithmakumi@gmail.com>
2022-03-27 23:01:27 +0300

    add model details.

--- mygallery/models.py
+++ mygallery/models.py
@@ -3,13 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharFiled(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
     
     def __str__(self):
         return self.name
 
 class Photo(models.Model):
-    name = models.CharFiled(max_length=100, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
     
     def __str__(self):
-        return self.name
+        return self.description
