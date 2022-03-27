220227d8c91745e55ad81b83f81cee615396e6b2
faithremo <faithmakumi@gmail.com>
2022-03-27 22:53:34 +0300

    add category and photo models

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharFiled(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    name = models.CharFiled(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
