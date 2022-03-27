b9eb0004c1f48953b1e8d6c231f1d4ba476f96fe
faithremo <faithmakumi@gmail.com>
2022-03-27 10:56:05 +0300

    add all the files

---
+++ mygallery/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns=[
+    path('',views.welcome,name = 'welcome'),
+    path('today/',views.mygallery_of_day,name='galleryToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_mygallery, name= 'pastPhotos'),
+]
+
+
+
---
+++ mygallery/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse, Http404
+import datetime as dt
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('My Gallery')
+
+
+def mygallery_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-photos/todays-photos.html', {"date": date,})
+    
+  
+def past_days_mygallery(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    if date == dt.date.today():
+        return redirect(mygallery_of_day)
+
+    return render(request, 'all-photos/past-photos.html', {"date": date})
+
+    
+    
+def welcome(request):
+    return render(request, 'welcome.html')
+
+    
+    
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
+
+
+
+
+
+
+
