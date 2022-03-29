2b0253587bd12459e4f157b1c30f9ab5a17f036c
faithremo <faithmakumi@gmail.com>
2022-03-30 00:29:49 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -181,7 +181,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
